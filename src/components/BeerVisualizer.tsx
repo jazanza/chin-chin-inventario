461678b6648720295acb2f7e2a8b08860ed7d3d4
[dyad] <git@dyad.sh>
2025-12-01 12:18:15 -0500

    Updated src/components/BeerVisualizer.tsx

--- src/components/BeerVisualizer.tsx
+++ src/components/BeerVisualizer.tsx
@@ -98,8 +98,8 @@ export function BeerVisualizer({ liters, visible, ...props }: { liters: number; 
 
       <Text
         ref={textRef}
-        position={[0, bottomY + 0.5, 0]}
-        fontSize={3}
+        position={[0, bottomY + 1, 0]}
+        fontSize={1}
         color="white"
         anchorX="center"
         anchorY="middle"
