0ca1bc3669b34cec4449f9f7c525cca1627a1724
[dyad] <git@dyad.sh>
2025-12-28 18:54:09 -0500

    [dyad] wrote 2 file(s)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="vite/client" />
-
-interface IElectronAPI {
-  openDbFile: () => Promise<Uint8Array | null>;
-}
-
-declare global {
-  interface Window {
-    electronAPI?: IElectronAPI;
-  }
-}
